249ed49642d291e46dd3d2ad410f172fd83efeb1
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-11-06 00:17:26 +0530

    Binded Request params to Model

--- src/main/java/com/springverse/thymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/springverse/thymeleaf/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -26,6 +27,18 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! " + theName;
+
+        model.addAttribute("message", result);
+        System.out.println("📳 model = " + model);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+        theName = theName.toUpperCase();
+
+        String result = "Hey my Friend from v3! " + theName;
 
         model.addAttribute("message", result);
         System.out.println("📳 model = " + model);
