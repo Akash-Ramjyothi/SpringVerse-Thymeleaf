95991d73a4602e253681ef2bacb4635c0529101a
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-11-07 00:10:16 +0530

    Created processForm() method

--- src/main/java/com/springverse/thymeleaf/controller/StudentController.java
+++ src/main/java/com/springverse/thymeleaf/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.springverse.thymeleaf.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,5 +18,12 @@ public class StudentController {
         System.out.println("🥝 theModel = " + theModel);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        System.out.println("🧢 theStudent = " + theStudent);
+
+        return "student-confirmation";
     }
 }
