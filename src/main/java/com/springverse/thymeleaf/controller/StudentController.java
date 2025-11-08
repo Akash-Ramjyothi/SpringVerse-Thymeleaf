56221127e1ea0d2a2ac191a9949af0f13cd22005
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-11-08 23:48:38 +0530

    Implemented property injection

--- src/main/java/com/springverse/thymeleaf/controller/StudentController.java
+++ src/main/java/com/springverse/thymeleaf/controller/StudentController.java
@@ -1,20 +1,27 @@
 package com.springverse.thymeleaf.controller;
 
 import com.springverse.thymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student theStudent = new Student();
 
         theModel.addAttribute("student", theStudent);
+        theModel.addAttribute("countries", countries);
         System.out.println("🥝 theModel = " + theModel);
 
         return "student-form";
